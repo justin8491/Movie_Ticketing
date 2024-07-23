563d3452cdddc6a462a3b25ae087e3cea98f56f7
justin8491 <justin77748292@gmail.com>
2024-07-23 10:55:23 +0900

    home_beta.jsp 포함 커밋

--- src/main/java/com/box/small/HomeController.java
+++ src/main/java/com/box/small/HomeController.java
@@ -16,9 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-
     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-
     /**
      * Simply selects the home view to render by returning its name.
      */
@@ -31,7 +29,7 @@ public class HomeController {
         model.addAttribute("serverTime", formattedDate);
 //        return "home_backup";
 //        return "home";
-        return "home";
+        return "home_beta";
     }
 
 }
