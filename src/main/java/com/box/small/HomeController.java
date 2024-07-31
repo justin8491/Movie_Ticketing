65615a077f78a1a29960d4ae56365d46a98e6508
justin8491 <justin77748292@gmail.com>
2024-07-31 17:30:04 +0900

    이거시 마스터 머지

--- src/main/java/com/box/small/HomeController.java
+++ src/main/java/com/box/small/HomeController.java
@@ -27,35 +27,33 @@ import com.box.small.user.movie.MovieService;
 
 @Controller
 public class HomeController {
-    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-    /**
-     * Simply selects the home view to render by returning its name.
-     */
-    
-    @Autowired
-    private MovieService service;
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
 
-    
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+	@Autowired
+	private MovieService service;
+
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView selectAllmovie() throws SQLException {
-		
+
 		System.out.println("메인 페이지로 이동");
-		
 		ModelAndView mav = new ModelAndView();
-		
 		List<MovieDto>movieList = service.selectAllMovieLive();
-		
+
 		List<CategoryDto>category = service.allCategory();
 		mav.addObject("movieList", movieList);
 		mav.addObject("category",category);
 		mav.setViewName("home_beta");
 		return mav;
 	}
-    
-    @RequestMapping(value = "/admin", method = RequestMethod.GET)
-    public String adminhome() {
-        return "homeAdmin_beta";
-    }
-    
+
+	@RequestMapping(value = "/admin", method = RequestMethod.GET)
+	public String adminhome() {
+		return "homeAdmin_beta";
+	}
+
 
 }
